8bd4c9badf4e0bc9d647dbc00f926de8f96d28bf
Jason Paul <jasonpa@gmail.com>
2022-05-15 17:09:17 -0400

    May 15 Updates

--- Debian-11-Bullseye/scripts/setup_debian.sh
+++ Debian-11-Bullseye/scripts/setup_debian.sh
@@ -15,13 +15,26 @@ fi
 echo '> Cleaning SSH keys ...'
 rm -f /etc/ssh/ssh_host_*
 
-#Generate a new SSH key for use with Terraform
-echo '> Generating new SSH key ...'
-ssh-keygen -A
-
 #Enable SSHD to start on boot
 echo '> Enabling sshd on startup ...'
+ssh-keygen -A
 systemctl enable sshd
+
+#Generate a new SSH key for use with Terraform
+echo '> Generating new SSH key for user debian and adding to Authorized Keys ...'
+mkdir $HOME/.ssh/
+ssh-keygen -t rsa -b 4096 -q -N "" -f $HOME/.ssh/id_rsa -C ""
+mkdir /home/debian/.ssh/tmp
+mv $HOME/.ssh/id_rsa* /home/debian/.ssh/
+cat /home/debian/.ssh/id_rsa.pub >> /home/debian/.ssh/authorized_keys
+chown -R debian:debian /home/debian/.ssh
+chmod 600 /home/debian/.ssh/authorized_keys
+chmod 600 /home/debian/.ssh/id_rsa*
+
+#Enable passwordless sudo for user debian
+echo '> Enabling passwordless sudo for user debian ...'
+echo 'debian ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/debian
+chmod 440 /etc/sudoers.d/debian
 
 # Sets hostname to localhost.
 echo '> Setting hostname to localhost ...'
